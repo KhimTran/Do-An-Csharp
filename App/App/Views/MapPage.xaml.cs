b263c88c50834c6f8c2e59b5a8d179a0f828dfd4
Vinh <vinhcong547@gmail.com>
2026-03-20 23:49:05 +0700

    tuần 2 Vinh

--- App/App/Views/MapPage.xaml.cs
+++ App/App/Views/MapPage.xaml.cs
@@ -1,10 +1,22 @@
-namespace App.Views
+using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+namespace App.Views;
+
+
+public partial class MapPage : ContentPage
 {
-    public partial class MapPage : ContentPage
+    public MapPage()
     {
-        public MapPage()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void BanDo_Loaded(object sender, EventArgs e)
+    {
+        BanDo.MoveToRegion(
+            Microsoft.Maui.Controls.Maps.MapSpan.FromCenterAndRadius(
+                new Location(10.757, 106.690),
+                Microsoft.Maui.Controls.Maps.Distance.FromKilometers(0.5)
+            )
+        );
     }
 }
